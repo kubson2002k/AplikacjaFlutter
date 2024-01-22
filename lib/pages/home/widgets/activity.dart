0f877c3a70a888f7bd68b23266db16888a4e2a6a
Jakub <jakub.kucharski@opoczta.pl>
2024-01-22 20:00:14 +0100

    Dodano nowy folder i pliki

--- lib/pages/home/widgets/activity.dart
+++ lib/pages/home/widgets/activity.dart
@@ -2,8 +2,16 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
-class RecentActivities extends StatelessWidget {
-  const RecentActivities({super.key});
+class RecentActivities extends StatefulWidget {
+  const RecentActivities({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _RecentActivitiesState createState() => _RecentActivitiesState();
+}
+
+class _RecentActivitiesState extends State<RecentActivities> {
+  List<ActivityData> activitiesData = List.generate(10, (index) => ActivityData.random());
 
   @override
   Widget build(BuildContext context) {
@@ -16,40 +24,101 @@ class RecentActivities extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              'Recent Activites',
-              style: Theme.of(context).textTheme.bodyLarge,
+            Row(
+              children: [
+                Text(
+                  'Recent Activities',
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+                const Spacer(),
+                IconButton(
+                  icon: const Icon(Icons.add, color: Colors.blue,),
+                  onPressed: () async {
+                    await _showAddActivityDialog();
+                  },
+                ),
+              ],
             ),
             Expanded(
               child: ListView.builder(
-                itemCount: 10,
-                itemBuilder: (context, index) => const ActivityItem(),
+                itemCount: activitiesData.length,
+                itemBuilder: (context, index) => ActivityItem(data: activitiesData[index]),
               ),
-            )
+            ),
           ],
         ),
       ),
     );
   }
+
+  Future<void> _showAddActivityDialog() async {
+    TextEditingController activityController = TextEditingController();
+    TextEditingController durationController = TextEditingController();
+    TextEditingController caloriesController = TextEditingController();
+
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Add New Activity', style: TextStyle(
+            fontSize: 30,
+          ),),
+          content: Column(
+            children: [
+              TextField(
+                controller: activityController,
+                decoration: const InputDecoration(labelText: 'Activity'),
+              ),
+              TextField(
+                controller: durationController,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(labelText: 'Duration (min)'),
+              ),
+              TextField(
+                controller: caloriesController,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(labelText: 'Calories'),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                String activity = activityController.text;
+                int duration = int.tryParse(durationController.text) ?? 0;
+                int calories = int.tryParse(caloriesController.text) ?? 0;
+
+                if (activity.isNotEmpty && duration > 0 && calories > 0) {
+                  ActivityData newActivity = ActivityData(activity: activity, duration: duration, calories: calories);
+                  setState(() {
+                    activitiesData.insert(0, newActivity); 
+                  });
+                  Navigator.of(context).pop(); 
+                } else {
+                }
+              },
+              child: const Text('Add'),
+            ),
+          ],
+        );
+      },
+    );
+  }
 }
 
 class ActivityItem extends StatelessWidget {
-  const ActivityItem({super.key});
+  final ActivityData data;
 
-  static const activities = [
-    'Running',
-    'Swimming',
-    'Hiking',
-    'Cycling',
-    'Walking',
-  ];
+  const ActivityItem({required this.data, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    String activity = activities[Random().nextInt(activities.length)];
-    int duration = Random().nextInt(31) + 30; 
-    int calories = Random().nextInt(301) + 200; 
-
     return GestureDetector(
       onTap: () {
         Navigator.of(context).pushNamed('/details');
@@ -86,7 +155,7 @@ class ActivityItem extends StatelessWidget {
             ),
             const SizedBox(width: 20),
             Text(
-              activity,
+              data.activity,
               style: const TextStyle(
                 fontSize: 12,
                 fontWeight: FontWeight.w900,
@@ -96,14 +165,14 @@ class ActivityItem extends StatelessWidget {
             const Icon(Icons.timer, size: 12),
             const SizedBox(width: 5),
             Text(
-              '$duration min',
+              '${data.duration} min',
               style: const TextStyle(fontSize: 10, fontWeight: FontWeight.w300),
             ),
             const SizedBox(width: 10),
             const Icon(Icons.wb_sunny_outlined, size: 12),
             const SizedBox(width: 5),
             Text(
-              '$calories kcal',
+              '${data.calories} kcal',
               style: const TextStyle(fontSize: 10, fontWeight: FontWeight.w300),
             ),
             const SizedBox(
@@ -113,5 +182,22 @@ class ActivityItem extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class ActivityData {
+  final String activity;
+  final int duration;
+  final int calories;
+
+  ActivityData({required this.activity, required this.duration, required this.calories});
+
+  factory ActivityData.random() {
+    List<String> activities = ['Running', 'Swimming', 'Hiking', 'Cycling', 'Walking'];
+    String activity = activities[Random().nextInt(activities.length)];
+    int duration = Random().nextInt(31) + 30;
+    int calories = Random().nextInt(301) + 200;
+
+    return ActivityData(activity: activity, duration: duration, calories: calories);
   }
 }
